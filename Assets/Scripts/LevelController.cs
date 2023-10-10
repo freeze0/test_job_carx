8240b6c1f61ec6e6bafe132fc0be622ea3b31737
freeze0 <kras2001@bk.ru>
2023-10-10 16:22:29 +0300

    stone collider
    добавил коллайдеры на камни и поменял размер

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -8,18 +8,29 @@ namespace Golf {
     {
         public StoneSpawner spawner;
         public bool isGameOver = false;
-        public float delay = 0.05f;
-    
+        public float delay = 0.5f;
+        // private float m_lastspawnedTime = 0;
+
         void Start()
         {
             StartCoroutine(SpawnStoneProc());
+            // m_lastspawnedTime = Time.time;
         }
+
+        /*private void Update()
+        {
+            if (isGameOver)
+            {
+                if (m_lastspawnedTime + delay > Time.time)
+                    m_lastspawnedTime = Time.time;
+            }
+        }*/
 
         private IEnumerator SpawnStoneProc()
         {
             do
             {
-                yield return new WaitForSeconds(delay);
+                yield return new WaitForSeconds(delay); // yield запоминает предыдущие состояния
                 spawner.Spawn();
             }
             while (!isGameOver);
